92abf7c606fa8fcfa828d64fd119ff668c8c8ee9
Александр <alexander9106@mail.ru>
2022-07-16 12:02:55 +0900

    Добавьте файлы проекта.

---
+++ Figures.cs
@@ -0,0 +1,14 @@
+﻿namespace Figures
+{
+    public class Figure
+    {
+        private IFigures figure;
+        public double Area => figure.Area;
+
+        public Figure() { }
+        public Figure(IFigures f)
+        {
+            figure = f;
+        }
+    }
+}
---
+++ Сircle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Сircle : IFigures
+
+    {
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set {
+                if (radius < 0)
+                    throw new ArgumentOutOfRangeException("Радиус должен быть больше нуля!!!");
+                radius = value;
+            }
+        }
+
+        public double Area { get; set; }
+        public Сircle() { }
+        public Сircle(double radius) 
+        { 
+            Radius = radius; 
+            Area = Math.PI * Math.Pow(radius, 2);
+        }
+     
+    }
+}
